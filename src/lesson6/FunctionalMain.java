f8c8d664d1654992317af61b2706f01395f4dca3
KH <nowukh@gmail.com>
2017-10-20 16:09:19 +0900

    lesson6 last commit

--- src/lesson6/FunctionalMain.java
+++ src/lesson6/FunctionalMain.java
@@ -1,6 +1,11 @@
 package lesson6;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FunctionalMain {
+
+
     public void main(String[] arg){
         Employee me = new Employee(2000), you = new Employee(5000);
         if (EmployeeTest.test(me, (m)->m.salary > 3000)) System.out.println("ok");
@@ -8,5 +13,11 @@ public class FunctionalMain {
 
         if (EmployeeTest.test(you, (m)->m.salary > 2000)) System.out.println("ok");
         else System.out.println("looser");
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add(null);
+        list.add("hika");
+        list.add(null);
+        list.add("KH");
     }
 }
